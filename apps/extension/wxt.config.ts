63b1859d528be32737460703db1e6efff714207c
Ollie <ohall2logs@gmail.com>
2026-05-03 23:05:17 +0100

    Firefox / Chrome Store Support

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -1,14 +1,35 @@
 import { defineConfig } from 'wxt';
 
+const firefoxDataCollectionPermissions = {
+  required: [
+    'personallyIdentifyingInfo',
+    'authenticationInfo',
+    'personalCommunications',
+    'browsingActivity',
+    'websiteContent',
+    'websiteActivity',
+  ],
+};
+
 export default defineConfig({
   vite: () => ({
     envPrefix: ['VITE_', 'PUBLIC_'],
   }),
-  manifest: {
+  manifest: ({ browser }) => ({
     name: 'Booth Reviews',
     description: 'Community reviews and ratings for Booth.pm listings.',
+    homepage_url: 'https://github.com/frog-enjoyer/booth-reviews',
     permissions: ['storage'],
     host_permissions: ['https://booth.pm/*', 'https://*.booth.pm/*'],
     default_locale: 'en',
-  },
+    ...(browser === 'firefox'
+      ? {
+          browser_specific_settings: {
+            gecko: {
+              data_collection_permissions: firefoxDataCollectionPermissions,
+            },
+          },
+        }
+      : {}),
+  }),
 });
